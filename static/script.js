d8f917f494b21f33af0337cd27d40fe810146909
Vadym Zelenko <115219323+01cheese@users.noreply.github.com>
2024-06-20 15:25:46 +0200

    script.js

--- static/script.js
+++ static/script.js
@@ -1,6 +1,7 @@
 const themeSwitcher = document.querySelector('.theme-switcher');
 const body = document.body;
 const themeKey = 'theme';
+const loadingBar = document.querySelector('.loading-bar');
 
 // Функция для сохранения текущей темы в локальное хранилище
 const saveThemeToLocalStorage = (theme) => {
@@ -30,12 +31,57 @@ themeSwitcher.addEventListener('click', () => {
     }
 });
 
+const showLoadingBar = () => {
+    loadingBar.style.display = 'block';
+    loadingBar.style.width = '0';
+    setTimeout(() => {
+        loadingBar.style.width = '100%';
+    }, 10); // Небольшая задержка для анимации
+};
 
+const hideLoadingBar = () => {
+    loadingBar.style.width = '0';
+    setTimeout(() => {
+        loadingBar.style.display = 'none';
+    }, 500); // Соответствует времени transition
+};
+
+const handleSearch = async (event) => {
+    event.preventDefault();
+    showLoadingBar();
+
+    const formData = new FormData(event.target);
+    const response = await fetch('/ajax_search', {
+        method: 'POST',
+        body: formData
+    });
+
+    const result = await response.json();
+    console.log(result);
+
+    const searchResultsContainer = document.querySelector('.search-results');
+    if (result.status === 'success') {
+        let html = `<h2>Search Results for '${result.query}':</h2>`;
+        result.results.forEach(item => {
+            html += `
+                <div class="search-result">
+                    <a href="${item.url}" class="result-title">${item.title}</a>
+                    <p class="result-snippet">${item.first_sentence}</p>
+                    <hr>
+                </div>
+            `;
+        });
+        searchResultsContainer.innerHTML = html;
+    } else {
+        searchResultsContainer.innerHTML = `<h2>Nothing found for '${result.query}'.</h2>`;
+    }
+    hideLoadingBar();
+};
 
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('search-form').addEventListener('submit', function(event) {
-        event.preventDefault(); // Предотвращаем отправку формы по умолчанию
-        // Здесь можно добавить дополнительную логику перед отправкой формы, если нужно
-        this.submit(); // Отправляем форму
-    });
+    document.getElementById('search-form').addEventListener('submit', handleSearch);
+
+    if (document.querySelector('.search-results')) {
+        document.querySelector('.search-results').addEventListener('submit', handleSearch);
+    }
 });
